5ebc2eecc6e3a8df359b928270677c190722d433
zsrbakova <zsrbakova@yahoo.com>
2019-07-12 10:09:51 +0200

    Override example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        MySubClass object=new MySubClass();
+        // write your code here
+        MySubClass object = new MySubClass();
         object.doTheThing();
     }
 }
