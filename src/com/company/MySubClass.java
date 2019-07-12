3c006696534b54090e64d690fb9220d64eb22989
zsrbakova <zsrbakova@yahoo.com>
2019-07-12 10:08:14 +0200

    Override example

---
+++ src/com/company/MySubClass.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class MySubClass extends MySuperClass{
+    @Override
+    public void doTheThing()
+    {
+        System.out.println("Do it differently");
+    }
+}
---
+++ src/com/company/MySuperClass.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class MySuperClass {
+    public void doTheThing()
+    {
+        System.out.println("Do the thing");
+    }
+}
+
